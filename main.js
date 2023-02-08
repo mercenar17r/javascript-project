a8e7e1e1dae2ddef8951358f45a03598eccfa0a0
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-02-08 23:35:18 +0200

    map

--- main.js
+++ main.js
@@ -31,26 +31,28 @@ let shopItemsData = [
 ];
 
 let generateShop = () => {
-  return (shop.innerHTML = `
-  <div class="item">
-  <img src="images/img-1.jpg" width="220" alt="" />
-  <div class="details">
-    <h3>Casual Shirt</h3>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-      Accusantium!
-    </p>
-    <div class="price-quantity">
-      <h2>$ 35</h2>
-      <div class="buttons">
-        <i class="bi bi-dash-lg"></i>
-        <div class="quantity">0</div>
-        <i class="bi bi-plus-lg"></i>
+  return (shop.innerHTML = shopItemsData.map((item) => {
+    return `
+    <div class="item">
+    <img src="images/img-1.jpg" width="220" alt="" />
+    <div class="details">
+      <h3>Casual Shirt</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Accusantium!
+      </p>
+      <div class="price-quantity">
+        <h2>$ 35</h2>
+        <div class="buttons">
+          <i class="bi bi-dash-lg"></i>
+          <div class="quantity">0</div>
+          <i class="bi bi-plus-lg"></i>
+        </div>
       </div>
     </div>
   </div>
-</div>
-    `);
+      `;
+  })).join();
 };
 
 generateShop();
