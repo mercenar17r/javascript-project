b9175960df8225a4ccde01bf1cb3caf74e8386e3
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-02-28 22:36:57 +0200

    clear item

--- main.js
+++ main.js
@@ -55,3 +55,29 @@ let calculation = () => {
 };
 
 calculation();
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let TotalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+
+        return item * search.price;
+      })
+      .reduce((x, y) => x + y, 0);
+    // console.log(amount);
+    label.innerHTML = `
+    <h2>Total Bill : $ ${amount}</h2>
+    <button class="checkout">Checkout</button>
+    <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+    `;
+  } else return;
+};
+
+TotalAmount();
