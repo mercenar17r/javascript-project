7252c3026f0a3743dffeb31cb3add1179f74512c
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-03-04 23:56:48 +0200

    cart js

---
+++ cart.js
@@ -0,0 +1,11 @@
+let label = document.getElementById("label");
+let ShoppingCart = document.getElementById("shopping-cart");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
