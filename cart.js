faa217a24ac8b627c45396326db9d7720014f9c2
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-03-07 23:45:42 +0200

    generate cart

--- cart.js
+++ cart.js
@@ -9,3 +9,37 @@ let calculation = () => {
 };
 
 calculation();
+
+let generateCartItems = () => {
+  return (ShoppingCart.innerHTML = basket
+    .map((x) => {
+      let { id, item } = x;
+      let search = shopItemsData.find((x) => x.id === id) || [];
+      let { img, price, name } = search;
+      return `
+      <div class="cart-item">
+        <img width="100" src=${img} alt="" />
+        <div class="details">
+        
+          <div class="title-price-x">
+            <h4 class="title-price">
+              <p>${name}</p>
+              <p class="cart-item-price">$ ${price}</p>
+            </h4>
+            <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+          </div>
+          <div class="cart-buttons">
+            <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+          <h3>$ ${item * price}</h3>
+        
+        </div>
+      </div>
+      `;
+    })
+    .join(""));
+};
